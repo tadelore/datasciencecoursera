1df294f0a2c4049cec3da739943f44298c834401
Temilade Sorungbe <temiadelore@gmail.com>
2016-04-05 11:47:47 +0100

    Adding Data Cleaning Course Project

---
+++ DataCleaning/Course Project/run_analysis.R
@@ -0,0 +1,69 @@
+#Getting and Cleaning Data Project 
+#set working directory
+setwd("C:/Users/datacent52/Documents/Temilade Adelore_Office/DataScienceCourse/DataCleaning")
+
+#include the following libraries
+library(plyr)
+library(dplyr)
+library("RCurl", lib.loc="~/R/win-library/3.2")
+
+#create object with file names with other datsets in train folder
+f_train <- file.path(paste(getwd(),"/UCI HAR Dataset/train", sep = ""),
+                     c("subject_train.txt", "X_train.txt", "y_train.txt"))
+
+#read train dataset
+dl_train <- lapply(f_train, read.table)
+
+#view train dataset
+str(dl_train)
+
+#Then create object with file names with other datsets in test folder
+f_test <- file.path(paste(getwd(),"/UCI HAR Dataset/test", sep = ""), 
+                    c("subject_test.txt", "X_test.txt", "y_test.txt"))
+
+#read train dataset
+dl_test <- lapply(f_test, read.table)
+
+#view train dataset
+str(dl_test)
+
+#merge training and test sets to creat one datset
+mergedData <- merge.list(dl_train, dl_test)
+
+#convert merged list to data frame
+mergedData <- do.call("cbind",mergedData)
+
+#read the features txt file (this contain variable names for the dataset)
+features <- read.table(paste(getwd(),"/UCI HAR Dataset/features.txt", sep = ""))
+
+#rename variables (columns) with list from features
+names(mergedData) <- c("Subject_test", as.character(features[,2]), "Activity")
+
+#view names
+head(names(mergedData))
+
+#replace Activity variable values with descriptives
+mergedData$Activity <- as.character(mergedData$Activity)
+mergedData$Activity <- revalue(mergedData$Activity, c("1" = "WALKING", "2" = "WALKING_UPSTAIRS",
+                                                      "3" = "WALKING_DOWNSTAIRS", "4" = "SITTING",
+                                                      "5" = "STANDING", "6" = "LAYING"))
+
+#view Activity variable 
+head((mergedData$Activity))
+
+#remove duplicate column names 
+rd_mergedData <- mergedData[,!duplicated(colnames(mergedData))]
+
+#extract the mean and standard deviation measurements for each variable 
+mean_mergedData <- select(rd_mergedData, contains("mean"))
+std_mergedData <- select(rd_mergedData, contains("std"))
+
+#create a dataset with the average of each variable for each activity and each subject
+newdata <- aggregate(mergedData[,2:562], list(mergedData$Subject_test, mergedData$Activity), mean)
+
+#save newdata as a text file
+write.table(newdata, file = paste(getwd(), "/Course Project/newdata_step5.txt",
+                                              sep = ""), row.names = FALSE)
+
+
+                         
